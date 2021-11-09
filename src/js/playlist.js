e31e45872b31b35ffe1053d2339dfdafde3bb0d5
AnastasiyaSam <anastasiya.samoilenka@hyperisland.se>
2021-11-09 18:57:32 +0100

    from the lecture  2021-11-9

--- src/js/playlist.js
+++ src/js/playlist.js
@@ -1,6 +1,73 @@
-/*
+function getAccessToken() {
+  const hash = window.location.hash;
+  const hashWithoutHash = hash.substring(1);
 
-  Follow the instructions for "Calling the Spotify playlist API" and
-  "Rendering a playlist visualisation" in the README 
+  const params = hashWithoutHash.split('&');
+  const keyValues = params.map((param) => param.split('='));
 
-*/
+  const accessToken = keyValues[0][1];
+  return accessToken;
+}
+
+function getPlaylist(playlist_id) {
+  const url = `https://api.spotify.com/v1/playlists/${playlist_id}`;
+  const headers = {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${getAccessToken()}`,
+  };
+
+  return fetch(url, { headers }).then((response) => response.json());
+}
+
+function renderPlaylist(playlistId) {
+  const container = document.getElementById('tracks');
+  const audioPlayer = document.getElementById('player');
+  getPlaylist(playlistId).then((playlist) => {
+    const tracks = playlist.tracks.items;
+
+    for (let i = 0; i < tracks.length; i++) {
+      const track = tracks[i].track;
+
+      const playListItem = document.createElement('div');
+      playListItem.classList.add('playlist-item');
+
+      const playListItemImg = document.createElement('img');
+      playListItemImg.classList.add('playlist-item-img');
+      playListItemImg.setAttribute('src', track.album.images[0].url);
+
+      const playListItemTitle = document.createElement('div');
+      playListItemTitle.classList.add('playlist-item-title');
+      playListItemTitle.innerHTML = track.name;
+
+      playListItem.addEventListener('click', () => {
+        if (currentlyActive === track.id) {
+          audioPlayer.pause();
+          currentlyActive = null;
+          playListItem.classList.remove('active');
+        } else {
+          if (currentlyActive) {
+            document.querySelector('.active').classList.remove('active');
+          }
+          currentlyActive = track.id;
+          playListItem.classList.add('active');
+        }
+        if (track.preview_url) {
+          currentlyActive = track.preview_url;
+          audioPlayer.setAttribute('src', track.preview_url);
+          audioPlayer.play();
+        } else {
+          audioPlayer.pause();
+        }
+      });
+
+      playListItem.appendChild(playListItemImg);
+      playListItem.appendChild(playListItemTitle);
+      container.appendChild(playListItem);
+    }
+  });
+}
+
+let currentlyActive;
+// https://open.spotify.com/show/15HFmorFE9BBuzImuymwdt?si=63f4b17708034a0a
+renderPlaylist('37i9dQZF1DWTMYgB8TqtmR');
